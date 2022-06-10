e0c317d90e89aa37fd6272cd113c238fc006084d
tsubomoto-risa <tsubomoto_risa@coreprice.com>
2022-06-10 20:02:15 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 坪本 梨沙 on 2022/06/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
